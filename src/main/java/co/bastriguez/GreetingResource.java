66802233443407e93f51531febd9cb7733a57b83
Pablo Bastidas <p.bastidas@quin.md>
2025-05-09 10:10:27 +0200

    Add userinfo endpoint and related tests for OIDC integration

--- src/main/java/co/bastriguez/GreetingResource.java
+++ src/main/java/co/bastriguez/GreetingResource.java
@@ -1,14 +1,24 @@
 package co.bastriguez;
 
+import io.quarkus.oidc.UserInfo;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import java.util.List;
 
 @Path("/")
+@Produces(MediaType.APPLICATION_JSON)
 public class GreetingResource {
+
+    @Inject
+    JsonWebToken accessToken;
+    @Inject
+    UserInfo userInfo;
+
 
     public record Checklist(boolean status, String description) {
     }
@@ -19,17 +29,19 @@ public class GreetingResource {
             new Checklist(true, "Checklist 3")
     );
 
-    @GET
-    @Path("/hello")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
-    }
-
     @Path("/checklists")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Checklist> getChecklists() {
         return checklists;
     }
+
+    @Path("/userinfo")
+    @GET
+    public UserInfoResponse getUserInfo() {
+        var pictureUrl = accessToken.claim("picture").orElse("/user-icon.svg").toString();
+        return new UserInfoResponse(accessToken.getSubject(), userInfo.getEmail(), pictureUrl, userInfo.getName());
+    }
+
+    public record UserInfoResponse(String id, String email, String pictureUrl, String fullName){}
 }
--- src/test/java/co/bastriguez/GreetingResourceTest.java
+++ src/test/java/co/bastriguez/GreetingResourceTest.java
@@ -2,13 +2,20 @@ package co.bastriguez;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
+import io.quarkus.test.security.oidc.Claim;
+import io.quarkus.test.security.oidc.OidcSecurity;
+import io.quarkus.test.security.oidc.UserInfo;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class GreetingResourceTest {
+
+    private static final Logger log = LoggerFactory.getLogger(GreetingResourceTest.class);
 
     @Test
     @TestSecurity(user = "alice")
@@ -20,4 +27,36 @@ class GreetingResourceTest {
                 .body("[0].status", is(true));
     }
 
+    @Test
+    @TestSecurity(user = "alice")
+    @OidcSecurity(
+            claims = {
+                    @Claim(key = "sub", value = "alice"),
+                    @Claim(key = "picture", value = "https://test.biz/alice.png"),
+            },
+            userinfo = {
+                    @UserInfo(key = "email", value = "alice@test.biz"),
+                    @UserInfo(key = "name", value = "Alice Test"),
+            }
+    )
+    void userinfoEndpoint(){
+        given()
+                .when().get("api/userinfo")
+                .then()
+                .statusCode(200)
+                .body("id", is("alice"))
+                .body("email", is("alice@test.biz"))
+                .body("fullName", is("Alice Test"))
+                .body("pictureUrl", is("https://test.biz/alice.png"));
+    }
+
+    @Test
+    @TestSecurity(user = "alice")
+    void defaultUserImage() {
+        given()
+                .when().get("api/userinfo")
+                .then()
+                .statusCode(200)
+                .body("pictureUrl", is("/user-icon.svg"));
+    }
 }
